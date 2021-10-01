08c9a3b650362a70c7831b42bc5aee69bc984de8
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-10-01 16:16:09 +0200

    struttura base Vue

---
+++ src/components/Albums.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <Album/>
+    </section>
+</template>
+
+<script>
+import Album from './Album.vue'
+
+
+
+export default {
+  components: { Album },
+name: "Albums",
+}
+</script>
+
+<style>
+
+</style>
